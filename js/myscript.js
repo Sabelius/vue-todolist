7c7912420ea29d55e3711ed63d8ebdabf3685e70
Sabelius <sbebby92@virgilio.it>
2021-09-16 15:50:02 +0200

    versione finale

--- js/myscript.js
+++ js/myscript.js
@@ -2,15 +2,24 @@ let root = new Vue({
     el: "#root",
 
     data: {
+        
         shoppingList : ["pane", "patate", "pasta integrale", "parmigiano", "banane", "mele", "riso basmati"],
+
+        newItem: "",
+
     },
 
-    
-    
     methods : {
 
         removeItem: function(indice){
             this.shoppingList.splice(indice, 1);
-        }
+        },
+
+        addNewItem: function(){
+            if(this.newItem.trim().length > 0 && isNaN(this.newItem)){
+                this.shoppingList.push(this.newItem);
+                this.newItem = "";
+            };
+        },
     },
 });
