4d81e13fee7b2f8d9cb2a5054332f4c3f1b5d4c9
Sabelius <sbebby92@virgilio.it>
2021-09-16 13:44:22 +0200

    inizializzazione esercizio

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+let root = new Vue({
+    el: "#root",
+
+    data: {
+        shoppingList : ["pane", "patate", "pasta integrale", "parmigiano", "banane", "mele", "riso basmati"],
+    },
+    
+    methods : {
+    },
+});
